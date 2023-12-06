a5dc9aef100e00acc591e29725ab7cd13f23a25a
Irfan Ali <151863237+Irfan-Ali01@users.noreply.github.com>
2023-12-06 21:41:21 +0500

    Add files via upload

---
+++ Basic_Calculator.py
@@ -0,0 +1,13 @@
+num1=int(input("Enter 1st Number: "))
+num2=int(input("Enter 2nd Number: "))
+Operator=(input("Enter The Operator(+,-,*,/): "))
+if Operator=="*":
+    print(num1*num2)
+elif Operator=="/":
+    print(num1/num2)
+elif Operator=="-":
+    print(num1-num2)
+elif Operator=="+":
+    print(num1+num2)
+else:
+    print("Invalid Operation")
